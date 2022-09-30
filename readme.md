ecc268f5431236117600e1ffbf0894dd06752fe8
yamaguchi-t <yamaguchi-t@roboken.iit.tsukuba.ac.jp>
2022-09-30 16:18:32 +0900

    joy操作等の修正

---
+++ readme.md
@@ -0,0 +1,48 @@
+# 直線追従
+
+1. ターミナルから
+ypspur-coordinator　を起動
+
+2. 別ターミナルで
+
+        $ roslaunch yamabico_pkg line_GL.launch
+
+### 各paramについて
+
+追従する直線が通る点(x,y)
+
+    <param name="point_x_double" value="2.0"/>
+    <param name="point_y_double" value="0.0"/>
+
+追従する直線の傾き(rad)
+
+    <param name="line_theta_radian" value="0.785"/>
+
+停止する位置(x,y)
+
+    <param name="goal_x_double" value="4.0"/>
+    <param name="goal_y_double" value="2.0"/>
+
+速度
+
+    <param name="max_speed" value="0.4"/>
+
+
+-----------------------------------------------------------------
+# カルガモ
+
+1. ターミナルから
+ypspur-coordinator　を起動
+
+2. 別ターミナルで
+
+        $ roslaunch yamabico_pkg urg_node.launch
+
+3. さらに別ターミナルで
+
+        $ rosrun yamabico_pkg karugamo
+
+
+
+
+※karugamo.launchはgazeboで動かすときに使用します
--- scripts/teleop_twist_joy.py
+++ scripts/teleop_twist_joy.py
@@ -9,8 +9,8 @@ class JoyTwist(object):
     def __init__(self):
         self._joy_sub = rospy.Subscriber('/joy', Joy, self.joy_callback, queue_size=1)
         self._twist_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
-        self.max_speed  = rospy.get_param('~max_speed', 0.1)
-        self.max_turn   = rospy.get_param('~max_turn', 0.1)
+        self.max_speed = rospy.get_param('~max_speed', 0.1)
+        self.max_turn = rospy.get_param('~max_turn', 0.1)
 
     def joy_callback(self, joy_msg):
 
@@ -28,11 +28,11 @@ class JoyTwist(object):
         # Rotational motion -----------------------------
         twist.angular.z = joy_msg.axes[3] * self.max_turn
 
-        #print twist
+        # print twist
         self._twist_pub.publish(twist)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
 
     rospy.init_node('teleop_twist_joy')
     joy_twist = JoyTwist()
